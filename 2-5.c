62ee008c05c58fe69c44cd6a87e2ada26fcdd0b3
lanchaoxiang <lcx@lcxdeMacBook-Pro.local>
2016-10-22 13:44:49 +0800

    自定义函数打印2

---
+++ 2-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void br(void);
+void ic(void);
+int main(void)
+{
+	br();
+	ic();
+	printf("India,China,\n");
+	printf("Brazil,Ruassia");
+}
+void br(void)
+{printf("Brazil,Ruassia,");}
+void ic(void)
+{printf("India,China,\n");}
